171a6902453a3ebdf202b782fee0d6623ed539a1
Brent Davies <brent@brentmdavies.com>
2019-05-02 15:31:45 -0600

    Adding fail2ban for extra SSH security before installing Salt

--- salt-bootstrap.sh
+++ salt-bootstrap.sh
@@ -1,4 +1,7 @@
-#Salt bootsrap and install
+#Salt bootstrap and install
+
+sudo apt-get update
+sudo apt-get install fail2ban
 
 curl -o bootstrap-salt.sh -L https://bootstrap.saltstack.com
 sudo sh bootstrap-salt.sh git develop
