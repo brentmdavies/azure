b4b8d7746aeac5679db42a297afeeb7116fbbb57
Brent Davies <brent@brentmdavies.com>
2019-05-05 11:20:35 -0600

    Rename from yml to config for salt consumption. Turns out salt is different from Ansible :-)

--- azure-salt-minion.sh
+++ azure-salt-minion.sh
@@ -29,3 +29,10 @@ vim /srv/salt/top.sls
 base:
   'myMinionVM':
     - nginx
+
+/etc/nginx/nginx.conf:
+  file.managed:
+    - source: salt://nginx/files/etc/nginx/nginx.conf
+    - user: root
+    - group: root
+    - mode: 640
